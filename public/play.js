27b04291a72e547044c4726b388f0bc541849e6b
MorningCoral <ng.lingshan@dhs.sg>
2019-03-02 19:02:11 +0800

    set up storage for question images

--- public/play.js
+++ public/play.js
@@ -13,3 +13,22 @@ firebase.auth().onAuthStateChanged(function(user) {
 var handleSignedInUser = function(user) {
     // scoring system
 }
+var storageRef = firebase.storage().ref();
+storageRef.child('qnimages/bomb.png').getDownloadURL().then(function(url) {
+  // `url` is the download URL for 'images/stars.jpg'
+
+  // This can be downloaded directly:
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'blob';
+  xhr.onload = function(event) {
+    var blob = xhr.response;
+  };
+  xhr.open('GET', url);
+  xhr.send();
+
+  // Or inserted into an <img> element:
+  var img = document.getElementById('qnimg');
+  img.src = url;
+}).catch(function(error) {
+  // Handle any errors
+});
